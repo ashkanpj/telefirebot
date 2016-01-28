10023908a2d9fa3d19ad22fb1a5172a83651e59f
ashkanpj <ashkanpj.ap33@gmail.com>
2016-01-28 14:08:55 +0330

    Delete echo.lua

--- plugins/echo.lua
+++
@@ -1,3 +0,0 @@
-local function run(msg, matches) local text = matches[1] local b = 1 while b ~= 0 do text = text:trim() text,b = text:gsub('^!+','') end return textend return { description = "Simplest plugin ever!", usage = "!echo [whatever]: echoes the msg", patterns = { "^!echo +(.+)$" }, run = run }
-
-Status API Training Shop Blog About Pricing© 2016 GitH
